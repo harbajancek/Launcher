627e406bb818fa4c4df53c0ba8e6fb7f0481f0e6
harbaja16 <harbaja16@DST20714>
2018-12-04 10:45:55 +0100

    změna k dobru

---
+++ LauncherLib/SearchExplorer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LauncherLib
+{
+    public class SearchExplorer
+    {
+        public string SearchDirectoryPath = @"D:\harbaja16\";
+
+        public void Search()
+        {
+            List<string> exePaths = new List<string>();
+
+            foreach (var item in Directory.GetFiles(SearchDirectoryPath, "*.csproj", SearchOption.AllDirectories))
+            {
+                
+
+                XDocument dox = XDocument.Parse(File.ReadAllText(item));
+
+                //var group = dox.Descendants().Descendants().Where(n => n.Attribute("Condition").Value == " '$(Configuration)|$(Platform)' == 'Release|AnyCPU' ").FirstOrDefault();
+
+                /*var basicProperty = dox
+                    .Descendants()
+                    .Descendants()
+                    .Where(
+                        n => n.Name.LocalName == "PropertyGroup" && 
+                        n.Descendants("AssemblyName").Any())
+                    .FirstOrDefault();*/
+
+                var basicProperty = dox
+                    .Descendants()
+                    .Descendants()
+                    .Where(
+                        n => n.Name.LocalName == "PropertyGroup" &&
+                        n.Descendants("AssemblyName").Any())// tohle nefunguje
+                    .FirstOrDefault();
+
+                var releaseProperty = dox
+                    .Descendants()
+                    .Descendants()
+                    .Where(
+                        n => n.Name.LocalName == "PropertyGroup" &&
+                        n.HasAttributes && n.Attribute("Condition").Value == " '$(Configuration)|$(Platform)' == 'Release|AnyCPU' ")
+                    .FirstOrDefault();
+
+                if (releaseProperty != null && basicProperty != null)
+                {
+                    var node = releaseProperty.Descendants().Where(n => n.Name.LocalName == "OutputPath").FirstOrDefault();
+
+                    if (node != null)
+                    {
+                        Console.WriteLine(Path.GetDirectoryName(item) + @"\" + node.Value + basicProperty.Descendants("AssemblyName").FirstOrDefault().Value);
+                    }
+                }
+            }
+            
+        }
+    }
+}
---
+++ LauncherTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using LauncherLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LauncherTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileHelper fileHelper = new FileHelper();
+            SearchExplorer searchExplorer = new SearchExplorer();
+
+            searchExplorer.Search();
+        }
+    }
+}
